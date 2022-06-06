506572f8d4b9ecc99b32ca1ff580c73f2dd8df07
idelc <idel012@ucr.edu>
2022-06-05 23:36:01 -0700

    basic java interface for testing and demo

---
+++ java/scripts/compile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export DB_NAME=$USER"_DB"
+DIR="$( cd "$( dirname "$BASH_SOURCE[0]}" )" && pwd)"
+# Indicate the path of the java compiler to use
+export JAVA_HOME=/usr/csshare/pkgs/jdk1.7.0_17
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Export classpath with the postgressql driver
+export CLASSPATH=$CLASSPATH:$PWD/../lib/pg73jdbc3.jar
+
+# compile the java program
+javac -d $DIR/../classes $DIR/../src/PingDatabase.java
+
+#run the java program
+#Use your database name, port number and login
+java -cp $DIR/../classes:$DIR/../lib/pg73jdbc3.jar PingDatabase $DB_NAME $PGPORT $USER
---
+++ java/scripts/test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+export DB_NAME=$USER"_DB"
+# Indicate the path of the java compiler to use
+export JAVA_HOME=/usr/csshare/pkgs/jdk1.7.0_17
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Export classpath with the postgressql driver
+export CLASSPATH=$CLASSPATH:$PWD/pg73jdbc3.jar
+
+# compile the java program
+javac PingDatabase.java
+
+#run the java program
+#Use your database name, port number and login
+java PingDatabase $USER"_DB" $PGPORT $USER
+
